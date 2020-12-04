eddb9c921ce890307acfb50e6b815617dea3b45c
samxoxoxo <rahibsam2@gmail.com>
2020-12-04 23:33:12 +0530

    Shooter Box Unity

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    public float motionMAgnitude = 0.1f;
+
+    public bool doSpin = false;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+            if(doSpin){
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+        if(doMotion){
+        gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMAgnitude );
+    }
+    }
+}
---
+++ Assets/Scripts/TargetMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMover : MonoBehaviour
+{
+    public enum motionDirections {Spin,Horizontal,Vertical};
+     public motionDirections motionState = motionDirections.Vertical;
+
+     public float spinSpeed = 180.0f;
+     public float motionMagnitude = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        switch(motionState){
+            case motionDirections.Spin: 
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+            break;
+            case motionDirections.Vertical: 
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+            break;
+            case motionDirections.Horizontal: 
+            gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+            break;
+        }
+    }
+}
